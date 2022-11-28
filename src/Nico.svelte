204fda024871e20e7dff995f16501264e0c52669
LAM Frédéric <flaaam@gmail.com>
2022-11-28 08:07:38 +0100

    Reshape all layout

--- src/Nico.svelte
+++ src/Nico.svelte
@@ -31,16 +31,19 @@ let infos:object = data.infos ;
       <div class="container-fluid position-relative">
           <div class="slider-active">
 
-              {#each about as a_bout}
+              {#each about as a_bout,i}
                 <div class="single-slider">
                     <div class="slider-bg">
                         <div class="row no-gutters align-items-center ">
                             <div class="col-lg-4 col-md-5">
                                 <div class="slider-product-image d-none d-md-block">
-                                    <img src="{a_bout.picture}" alt="{a_bout.title}">
-                                    <!--<div class="slider-discount-tag">
+                                    <!--<img src="{a_bout.picture}" alt="{a_bout.title}">
+                                    <div class="slider-discount-tag">
                                         <p>-50% <br> OFF</p>
                                     </div>-->
+                                    <div class="picture_">
+                                        Photo { i+1 }
+                                    </div>
                                 </div> <!-- slider product image -->
                             </div>
                             <div class="col-lg-8 col-md-7">
@@ -70,7 +73,7 @@ let infos:object = data.infos ;
           </div>
       </div> <!-- container fluid -->
   </section>
-
+  
   <section id="services" class="services-area pt-100 pb-130">
       <div class="container">
           <div class="row">
@@ -111,59 +114,6 @@ let infos:object = data.infos ;
       </div> <!-- container -->
   </section>
 
-  <section id="agenda">
-    
-    <div class="container">
-
-      <p style="margin:20px 0">De part mon activité, j’ouvre des plages à la reservation de massages à des moments precis. Voici les créneaux disponibles ci-dessous.</p>
-      <iframe title="agenda" src="https://calendar.google.com/calendar/embed?src=family07472837986232886021%40group.calendar.google.com&ctz=Europe%2FParis" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-
-    </div>
-
-  </section>
-
-  <section id="avis" class="blog-area pt-125">
-      <div class="container">
-          <div class="row">
-              <div class="col-lg-4">
-                  <div class="showcase-title pt-25">
-                      <h2 class="title">Avis</h2>
-                  </div> <!-- showcase title -->
-              </div> 
-          </div>
-          <div class="row justify-content-center">
-              
-              {#each avis as av}
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-blog mt-30">
-                        <div class="blog-content">
-                            <div class="content">
-                                <h4 class="title">"{av.title}"</h4>
-                                <span>{ av.date }</span>
-                            </div>
-                            <div class="meta d-flex justify-content-between align-items-center">
-                                <div class="meta-admin d-flex align-items-center">
-                                   
-                                    <div class="admin-title">
-                                        <h6 class="title"><a href="/">{ av.author }</a></h6>
-                                    </div>
-                                </div>  <!-- meta admin -->
-                                <div class="meta-icon">
-                                    <ul>
-                                        <li><a href="/"><i class="lni-share"></i></a></li>
-                                        <li><a href="/"><i class="lni-heart"></i></a></li>
-                                    </ul>
-                                </div> <!-- meta icon -->
-                            </div> <!-- meta -->
-                        </div>
-                    </div> <!-- single blog -->
-                </div>
-              {/each}
-
-          </div> <!-- row -->
-      </div> <!-- container -->
-  </section>
-
   <section id="contact" class="contact-area pt-115">
     <div class="container">
         <div class="row">
@@ -175,88 +125,23 @@ let infos:object = data.infos ;
           </div>
         <div class="contact-box mt-70">
             <div class="row">
-                <div class="col-lg-4">
-                    <div class="contact-info pt-25">
-                        <h4 class="info-title">Informations de contact</h4>
-                        <ul>
-                            <li>
-                                <div class="single-info mt-30">
-                                    <div class="info-icon">
-                                        <i class="lni-phone-handset"></i>
-                                    </div>
-                                    <div class="info-content">
-                                        <p><a href="tel:{infos.phone}">{infos.phone}</a></p>
-                                    </div>
-                                </div> <!-- single info -->
-                            </li>
-
-                            <li>
-                                <div class="single-info mt-30">
-                                    <div class="info-icon">
-                                        <i class="lni lni-whatsapp"></i>
-                                    </div>
-                                    <div class="info-content">
-                                        <p><a href="https://wa.me/{infos.phone}">{infos.phone}</a></p>
-                                    </div>
-                                </div> <!-- single info -->
-                            </li>
-
-                            <li>
-                                <div class="single-info mt-30">
-                                    <div class="info-icon">
-                                        <i class="lni-envelope"></i>
-                                    </div>
-                                    <div class="info-content">
-                                        <p><a href="mailto:{infos.email}">{infos.email}</a></p>
-                                    </div>
-                                </div> <!-- single info -->
-                            </li>
-                            <li>
-                                <div class="single-info mt-30">
-                                    <div class="info-icon">
-                                        <i class="lni-home"></i>
-                                    </div>
-                                    <div class="info-content">
-                                        <p>{infos.address}</p>
-                                    </div>
-                                </div> <!-- single info -->
-                            </li>
-                        </ul>
-                    </div> <!-- contact info -->
-                </div> 
-                <div class="col-lg-8">
-                    <div class="contact-form">
-                        <form id="contact-form" action="assets/contact.php" method="post" data-toggle="validator">
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="single-form form-group">
-                                        <input type="text" name="name" placeholder="Entrez votre nom" data-error="Name is required." required="required">
-                                        <div class="help-block with-errors"></div>
-                                    </div> <!-- single form -->
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="single-form form-group">
-                                        <input type="email" name="email" placeholder="Entrez votre email"  data-error="Valid email is required." required="required">
-                                        <div class="help-block with-errors"></div>
-                                    </div> <!-- single form -->
-                                </div>
-                                <div class="col-lg-12">
-                                    <div class="single-form form-group">
-                                        <textarea name="message" placeholder="Votre message" data-error="Please,leave us a message." required="required"></textarea>
-                                        <div class="help-block with-errors"></div>
-                                    </div> <!-- single form -->
-                                </div>
-                                <p class="form-message"></p>
-                                <div class="col-lg-12">
-                                    <div class="single-form form-group">
-                                        <button class="main-btn" type="submit">Envoyer</button>
-                                    </div> <!-- single form -->
-                                </div>
-                            </div> <!-- row -->
-                        </form>
-                    </div> <!-- row -->
-                </div> 
-            </div> <!-- row -->
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="contact_">
+                        <span><i class="lni-phone-handset"></i></span>
+                          <div class="footer-info-content">
+                              <p><a href="tel:{infos.phone}">{infos.phone}</a></p>
+                          </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="contact_">
+                        <span><i class="lni-phone-handset"></i></span>
+                          <div class="footer-info-content">
+                              <p><a href="tel:{infos.phone}">{infos.phone}</a></p>
+                          </div>
+                    </div>
+                </div>
+            </div>
         </div> <!-- contact box -->
     </div> <!-- container -->
   </section>
@@ -265,20 +150,21 @@ let infos:object = data.infos ;
       <div class="container">
           <div class="footer-widget pt-75 pb-120">
               <div class="row">
-                  <div class="col-lg-6 col-md-6 col-sm-6">
+                
+                  <!--<div class="col-lg-4 col-md-4 col-sm-6">
                       <div class="footer-logo mt-40">
                           <a href="/">
                               <img style="max-height: 80px;" src="assets/images/logo.png" alt="Logo">
                           </a>
-                          <p class="mt-10">Nicolas Berteyac, masseur, de père en fils depuis 1876</p>
+                          <p class="mt-10">Nicolas Berteyac</p>
                           <ul class="footer-social mt-25">
                               <li><a href="/"><i class="lni-facebook-filled"></i></a></li>
                               <li><a href="/"><i class="lni-twitter-original"></i></a></li>
                               <li><a href="/"><i class="lni-instagram"></i></a></li>
                           </ul>
-                      </div> <!-- footer logo -->
+                      </div>
                   </div>
-                  <!--<div class="col-lg-3 col-md-4 col-sm-5">
+                  <div class="col-lg-3 col-md-4 col-sm-5">
                       <div class="footer-link mt-50">
                           <h5 class="f-title">Services</h5>
                           <ul>
@@ -298,9 +184,8 @@ let infos:object = data.infos ;
                           </ul>
                       </div>
                   </div>-->
-                  <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="col-lg-12 col-md-12 col-sm-6">
                       <div class="footer-info mt-50">
-                          <h5 class="f-title">Informations de contact</h5>
                           <ul>
                               <li>
                                   <div class="single-footer-info mt-20">
@@ -318,14 +203,7 @@ let infos:object = data.infos ;
                                       </div>
                                   </div> <!-- single footer info -->
                               </li>
-                               <li>
-                                  <div class="single-footer-info mt-20">
-                                      <span><i class="lni lni-facebook-messenger"></i></span>
-                                      <div class="footer-info-content">
-                                          <p><a href="http://m.me/{infos.facebook}">{infos.facebook}</a></p>
-                                      </div>
-                                  </div> <!-- single footer info -->
-                              </li>
+                              
                               <li>
                                   <div class="single-footer-info mt-20">
                                       <span><i class="lni lni-envelope"></i></span>
@@ -351,7 +229,7 @@ let infos:object = data.infos ;
               <div class="row">
                   <div class="col-lg-12">
                       <div class="copyright text-center">
-                          <p>Made with <a href="https://vitejs.dev/">ViteJS <img src="{viteLogo}" alt="Vite JS"></a>, on top of <a href="https://svelte.dev/" target="_blank">Svetle <img src="{svelteLogo}" alt="Svelte"></a>, theme from <a href="https://uideck.com" rel="nofollow">UIdeck</a></p>
+                          <p>© 2023</p>
                       </div> <!-- copyright -->
                   </div>
               </div> <!-- row -->
